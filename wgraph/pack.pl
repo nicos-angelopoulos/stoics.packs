ee3ce0848c6dfcba36cb3868f4f0ed9ff68a69af
Nicos Angelopoulos <nicos.angelopoulos@gmail.com>
2019-04-21 19:25:07 +0100

    VERS:  0.5 tiny fix

--- wgraph/pack.pl
+++ wgraph/pack.pl
@@ -1,6 +1,6 @@
 name( wgraph ).
 title( 'Weighted graphs, with plotting via Real' ).
-version( '0.4' ).
+version( '0.5' ).
 author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
