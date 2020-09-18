89cc1154baa30573db2fc9e1a58e4ae090bb1f59
Nicos Angelopoulos <nicos@gmx.co.uk>
2020-09-18 23:46:53 +0100

    VERS:  0:5 @ 2020/9/18

--- b_real/src/b_real_version.pl
+++ b_real/src/b_real_version.pl
@@ -4,9 +4,12 @@ Version (Mj:Mn:Fx) and date and of publication (date(Y,M,D))>
 
 ==
 ?- version( Vers, Date ).
-Vers = 0:4:0,
-Date = date(2017, 4, 25).
-
+Vers = 0:5:0,
+Date = date(2020, 9, 18).
 ==
+
+@author nicos angelopoulos
+@version  0:4 2017/4/25
+
 */
-b_real_version( 0:4:0, date(2017,4,25) ).
+b_real_version( 0:5:0, date(2020,9,18) ).
