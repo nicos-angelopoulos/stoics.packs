9f96f49e82653cc034a3e6d3fcec07c4e4d1ab7d
Nicos Angelopoulos <nicos@gmx.co.uk>
2021-09-02 09:30:57 +0100

    VERS:   0.7

--- b_real/src/b_real_version.pl
+++ b_real/src/b_real_version.pl
@@ -1,15 +1,17 @@
 /** b_real_version( -Version, -Date ).
 
-Version (Mj:Mn:Fx) and date and of publication (date(Y,M,D))>
+Version (Mj:Mn:Fx) and date and of publication (date(Y,M,D)).
 
 ==
 ?- version( Vers, Date ).
-Vers = 0:6:0,
-Date = date(2021, 1, 23).
+Vers = 0:7:0,
+Date = date(2021, 9, 2).
 ==
 
 @author nicos angelopoulos
+@version  0:7 2021/9/2,   scripts/pack_dnloads.pl
+@version  0:6 2021/1/23
 @version  0:4 2017/4/25
 
 */
-b_real_version( 0:6:0, date(2021,1,23) ).
+b_real_version( 0:7:0, date(2021,9,2) ).
