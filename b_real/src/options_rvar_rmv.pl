9730186474dcc430c51852b7d23a55a34d0f2912
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-16 14:15:29 +0000

    IFACE:    options_rvar_rmv/2 Rvar can now be a list of R variables

--- b_real/src/options_rvar_rmv.pl
+++ b_real/src/options_rvar_rmv.pl
@@ -1,13 +1,13 @@
 
 :- use_module( library(real) ).
+:- use_module( library(options) ).
 % :- lib(options).  % options_debug/3.
 
-/** options_rvar_rmv( +Rvar, +Opts ).
+/** options_rvar_rmv( +RvarS, +Opts ).
 
-r_remove/1 R variable Rvar iff rvar_rmv(true) is in Opts.
+r_remove/1 R variable Rvar, or list of RvarS,  iff rvar_rmv(true) is in Opts.
  
-The rationale is to make rvar_rmv(_) a commonly occured option
-in b_real predicates.
+The rationale is to make rvar_rmv(_) a commonly occured option in b_real predicates.
 
 ==
 ?- x <- 1.
@@ -22,11 +22,19 @@ true.
 ?- options_rvar_rmv( x, [rvar_rmv(false),debug(true)] ).
 % Keeping R variable: x
 true.
+
+?- y <- 2.
+?- options_rvar_rmv( [x,y], [rvar_rmv(true)] ).
+true.
+
+?- <- x.
+ERROR: R was unable to digest your statement, either syntax or existance error.
 ==
 
 @author nicos angelopoulos
 @version  0.1 2015/1/15
-@tbd debug messages via options_debug/3 library(options_debug)
+@version  0.2 2022/12/16,  can operate on lists of Rvars now
+@tbd debug messages via options_debug/3 library(options)
 */
 
 options_rvar_rmv( Rvar, Opts ) :-
@@ -38,7 +46,13 @@ options_rvar_rmv( _Rvar, _Opts ).
 
 option_boolean_rvar_rmv( true, Rvar, Opts ) :-
 	!,
-	options_debug( 'Removing R variable: ~w', [Rvar], Opts ),
-	r_remove( Rvar ).
+     ( is_list(Rvar) -> Rvars=Rvar; Rvars=[Rvar] ),
+     option_boolean_rvar_rmv_list( Rvars, Opts ).
 option_boolean_rvar_rmv( _, Rvar, Opts ) :-
 	options_debug( 'Keeping R variable: ~w', [Rvar], Opts ).
+
+option_boolean_rvar_rmv_list( [], _Opts ).
+option_boolean_rvar_rmv_list( [Rv|Rvs], Opts ) :-
+	options_debug( 'Removing R variable: ~w', [Rv], Opts ),
+	r_remove( Rv ),
+     option_boolean_rvar_rmv_list( Rvs, Opts ).
