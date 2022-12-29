825fb8c521f43605f2c2611710fd6a67f69d0ad9
Nicos Angelopoulos <nicos@gmx.co.uk>
2022-12-29 16:56:53 +0000

    Vers:   v0.5

--- mlu/pack.pl
+++ mlu/pack.pl
@@ -1,6 +1,6 @@
 name( mlu ).
 title( 'Machine learning utilities' ).
-version( '0.4' ).
+version( '0.5' ).
 author( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 packager( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
 maintainer( 'Nicos Angelopoulos', 'http://stoics.org.uk/~nicos' ).
