a59d542384c7dd7424f575b260b6be4f669f0b66
Nicos Angelopoulos <nicos@gmx.co.uk>
2023-09-01 08:53:00 +0100

    CHANGED:  moved lolli to lolly and created lolli appropriate for not flipped lollipop plots

--- b_real/src/gg_theme.pl
+++ b_real/src/gg_theme.pl
@@ -14,7 +14,10 @@ Panel
   no theme, (empty list of terms)
 
   * lolli
-  theme suitable for lollipop plot (see gg_lollipop/2), light theme + no borders, no major and ticks on y 
+  theme suitable for lollipop plots (gg_lollipop/2) in normal orientation, light theme + no borders, no major and ticks on y 
+
+  * lolly
+  theme suitable for lollipop plots (gg_lollipop/2) in flipped orientation, light theme + no borders, no major and ticks on y 
 
   * standard
   standard panel decorations, i.e. no theme (empty list)
@@ -31,7 +34,7 @@ Panel
 
 @author nicos angelopoulos
 @version  0:2 2020/7/27, renamed and added to b_real (from hrmn), new theme
-@version  0:3 2023/8/31, added lolli theme (and aliased false to [])
+@version  0:3 2023/8/31, added lolli,lolly themes (and aliased false to [])
 
 */
 gg_theme( axes, List ) :-
@@ -51,6 +54,13 @@ gg_theme( blank, List ) :-
 % default theme for lollipop plots: see 
 gg_theme( false, [] ).
 gg_theme( lolli, List ) :-
+     List = [ theme_light(),
+              theme( 'panel.grid.major.x'=element_blank(),
+                     'panel.border'=element_blank(),
+                     'axis.ticks.x'=element_blank()
+                   )
+            ].
+gg_theme( lolly, List ) :-
      List = [ theme_light(),
               theme( 'panel.grid.major.y'=element_blank(),
                      'panel.border'=element_blank(),
