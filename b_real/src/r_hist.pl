3f8e108ba6c0157e917a770ba78c4104459d371b
Nicos Angelopoulos <nicos@gmx.co.uk>
2024-03-23 10:18:23 +0000

    FIXED:   adjust multi density plots to dimensions that cover all values

--- b_real/src/r_hist.pl
+++ b_real/src/r_hist.pl
@@ -69,6 +69,11 @@ Produces file: rh2.svg
                xlab="x_lab",ylab="y_lab",main="main", ylim=c(0,0.3)
              ],
      r_hist([rnsm2,rnsm],Fulls).
+?-
+     Fulls = [ multi(true), as_density(true), transparency_colour(["lightgreen","lightblue"]),
+               xlab="x_lab",ylab="y_lab",main="main", ylim=c(0,0.3), outputs(svg)
+             ],
+     r_hist([rnsm2,rnsm],Fulls).
 ==
 
 @author nicos angelopoulos
@@ -86,33 +91,53 @@ r_hist( VectIn, Args ) :-
         options( transparent(Trans), Opts ),
         options( transparency_colour(Clrs), Opts ),
         options( multi(Multi), Opts ),
-        r_dense( Multi, Trans, Clrs, Vect, Rcall, Post, Opts ),
+        r_dense( Multi, Trans, Clrs, Vect, Rcall, Post, DOpts, Opts ),
+        % ylim=c(0,0.3)
         % r_dense( Multi, Trans, Clrs, Vect, Goal, Post )
-        RcOpts = [post_call(Post)|NmOpts]
+        append( [post_call(Post)|NmOpts], DOpts, RcOpts )
         ;
         Rcall = hist(Vect),
         NmOpts = RcOpts
     ),
     r_call( Rcall, RcOpts ).
 
-r_dense( true, Trans, ClrS, VectS, Rcall, Post, Opts ) :-
+r_dense( true, Trans, ClrS, VectS, Rcall, Post, LOpts, Opts ) :-
      ClrS = [Clr|Clrs],
      VectS = [Vect|Vects],
-     r_dense( false, Trans, Clr, Vect, Acall, Apost, Opts ),
-     r_dense_multi( Vects, Clrs, Trans, Acall, Apost, Rcall, Post ).
-r_dense( false, Trans, Clr, Vect, Rcall, Post, _Opts ) :-
+     r_dense( false, Trans, Clr, Vect, Acall, Apost, _, Opts ),
+     % fixme: get unique R variable
+     v_dens <- density(Vect),
+     Xx <- max(v_dens$x),
+     Nx <- min(v_dens$x),
+     Xy <- max(v_dens$y),
+     Ny <- min(v_dens$y),
+     r_dense_multi( Vects, Clrs, Trans, Acall, Apost, r(Xx,Nx,Xy,Ny), Rcall, Post, r(Xxr,Nxr,Xyr,Nyr) ), 
+     LOpts = [xlim=c(Nxr,Xxr),ylim=c(Nyr,Xyr)].
+     % ROpts = [
+r_dense( false, Trans, Clr, Vect, Rcall, Post, [], _Opts ) :-
      r_hist_density_post( Trans, Clr, Vect, Post ),
      Rcall = plot(density(Vect)).
      % r_call( plot(density(Vect)), [post_call(Post)|Opts] ).
 
-r_dense_multi( [], [], _Trans, Acall, Apost, Rcall, Post ) :-
+r_dense_multi( [], [], _Trans, Acall, Apost, Rngs, Rcall, Post, Rngs ) :-
      Acall = Rcall,
-     Apost = Post.
-r_dense_multi( [V|Vs], [Clr|Clrs], Trans, Acall, Apost, Rcall, Post ) :-
+     Apost = Post,
+     r_remove( v_dens ).
+r_dense_multi( [V|Vs], [Clr|Clrs], Trans, Acall, Apost, Rngs, Rcall, Post, Ongs ) :-
      Bcall = (Acall,lines(density(V))),
      r_hist_density_post( Trans, Clr, V, Cpost ),
      Bpost = (Apost,Cpost),
-     r_dense_multi( Vs, Clrs, Trans, Bcall, Bpost, Rcall, Post ).
+     Rngs = r(Xxa,Nxa,Xya,Nya),
+     v_dens <- density(V),
+     Xxb <- max(v_dens$x),
+     Nxb <- min(v_dens$x),
+     Xyb <- max(v_dens$y),
+     Nyb <- min(v_dens$y),
+     Xxc is max(Xxa,Xxb),
+     Nxc is min(Nxa,Nxb),
+     Xyc is max(Xya,Xyb),
+     Nyc is min(Nya,Nyb),
+     r_dense_multi( Vs, Clrs, Trans, Bcall, Bpost, r(Xxc,Nxc,Xyc,Nyc), Rcall, Post, Ongs ).
 
 r_hist_density_post( true, Clr, Vect, Post ) :-
     Post  = ( <- polygon(density(Vect),col=adjustcolor(Clr,'alpha.f'=0.2),border=adjustcolor(Clr,alpha.f=0.8)) ).
