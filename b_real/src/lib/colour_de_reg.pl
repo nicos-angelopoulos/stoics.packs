428eaf38b3ea33f6e52bf0ddb377524353acda22
Nicos Angelopoulos <nicos@gmx.co.uk>
2025-09-27 21:45:30 +0100

    IFACE:    colour_de_reg/1,colour_de_reg_3/1 on module interface + promised RColorBrewer

--- b_real/src/lib/colour_de_reg.pl
+++ b_real/src/lib/colour_de_reg.pl
@@ -6,7 +6,7 @@
 %% colour_de_reg_3( -DeReg3 ).
 % 
 %  Standard colour for de regulation: DeReg = up_down(Up,Down)
-%  of deReg3 = up_down(Up,Level,Down)
+%  of DeReg3 = up_down(Up,Level,Down)
 %  Colours for up and down (de) regulation.
 %
 %==
@@ -15,12 +15,11 @@
 %  @author nicos angelopoulos
 %  @version 0.1 2014/02/12
 %  @version 0.2 2014/02/12,  switched the order, now: up = Red, down = Green.
-%
+%  @Version 0.3 2025/09/27,  dependncy to RColorBrewer is promised now, also exposed preds to module interface
 %
 colour_de_reg( up_down(Red,Green) ) :-
 	Set1 <- brewer.pal(9,"Set1"), 
 	Set1 = [Red,_Blue,Green|_].
-% colour_de_reg( up_down(Green,Red) ) :-
 
 colour_de_reg_3( up_down(Red,Orange,Green) ) :-
 	Set1 <- brewer.pal(9,"Set1"), 
