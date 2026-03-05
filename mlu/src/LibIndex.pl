b288d9e465b935753602c8798af3bd63b08b8ba8
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-05 01:09:09 +0000

    LIBidx:  mlu (created)

---
+++ mlu/src/LibIndex.pl
@@ -0,0 +1,17 @@
+% date(2026,3,5)
+
+lib_index(k_fold_pairwise_comparisons, 6, any, mlu, '../src/k_fold_pairwise_comparisons').
+lib_index(k_fold_pairwise_comparisons, 6, any, mlu, k_fold_pairwise_comparisons).
+lib_index(k_fold_pairwise_predictions, 6, any, mlu, '../src/k_fold_pairwise_predictions').
+lib_index(k_fold_pairwise_predictions, 6, any, mlu, k_fold_pairwise_predictions).
+lib_index(k_fold_segments, 4, any, mlu, '../src/k_fold_segments').
+lib_index(k_fold_segments, 4, any, mlu, k_fold_segments).
+lib_index(mlu_errors, 0, any, mlu, '../src/mlu_errors').
+lib_index(mlu_errors, 0, any, mlu, mlu_errors).
+lib_index(mlu_frequency_plot, 2, any, mlu, '../src/mlu_frequency_plot').
+lib_index(mlu_frequency_plot, 2, any, mlu, mlu_frequency_plot).
+lib_index(mlu_sample, 4, any, mlu, '../src/mlu_sample').
+lib_index(mlu_sample, 4, any, mlu, mlu_sample).
+lib_index(mlu_sample, 5, any, mlu, '../src/mlu_sample').
+lib_index(mlu_sample, 5, any, mlu, mlu_sample).
+lib_index(mlu_version, 2, any, mlu, '../prolog/mlu').
