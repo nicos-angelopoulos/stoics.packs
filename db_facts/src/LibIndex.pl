67dae0e486bace7e365e712c16e1a549f558b897
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-05 01:08:13 +0000

    LIBidx:  db_facts (created)

---
+++ db_facts/src/LibIndex.pl
@@ -0,0 +1,24 @@
+% date(2026,3,5)
+
+lib_index(db_assert, 1, any, db_facts, '../prolog/db_facts').
+lib_index(db_assert, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_assert, 3, any, db_facts, '../prolog/db_facts').
+lib_index(db_create, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_current_connection, 1, any, db_facts, '../prolog/db_facts').
+lib_index(db_current_connection, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_date_sql_atom, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_disconnect, 1, any, db_facts, '../prolog/db_facts').
+lib_index(db_goal_connection, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_holds, 1, any, db_facts, '../prolog/db_facts').
+lib_index(db_holds, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_max, 4, any, db_facts, '../prolog/db_facts').
+lib_index(db_min, 4, any, db_facts, '../prolog/db_facts').
+lib_index(db_query, 3, any, db_facts, '../prolog/db_facts').
+lib_index(db_retractall, 1, any, db_facts, '../prolog/db_facts').
+lib_index(db_retractall, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_retractall, 3, any, db_facts, '../prolog/db_facts').
+lib_index(db_table, 2, any, db_facts, '../prolog/db_facts').
+lib_index(db_table, 3, any, db_facts, '../prolog/db_facts').
+lib_index(db_table_column, 3, any, db_facts, '../prolog/db_facts').
+lib_index(db_table_column, 4, any, db_facts, '../prolog/db_facts').
+lib_index(db_version, 2, any, db_facts, '../prolog/db_facts').
