e2af1034bc113b145efd93419527b16ac0ce4542
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-05 01:10:07 +0000

    LIBidx:    for wgraph (created)

---
+++ wgraph/src/LibIndex.pl
@@ -0,0 +1,35 @@
+% date(2026,3,5)
+
+lib_index(wgraph, 2, any, wgraph, '../src/wgraph').
+lib_index(wgraph, 2, any, wgraph, wgraph).
+lib_index(wgraph, 3, any, wgraph, '../src/wgraph').
+lib_index(wgraph, 3, any, wgraph, wgraph).
+lib_index(wgraph_add_edges, 3, any, wgraph, '../src/wgraph_add_edges').
+lib_index(wgraph_add_edges, 3, any, wgraph, wgraph_add_edges).
+lib_index(wgraph_add_vertices, 3, any, wgraph, '../src/wgraph_add_vertices').
+lib_index(wgraph_add_vertices, 3, any, wgraph, wgraph_add_vertices).
+lib_index(wgraph_adjacency, 2, any, wgraph, '../src/wgraph_adjacency').
+lib_index(wgraph_adjacency, 2, any, wgraph, wgraph_adjacency).
+lib_index(wgraph_adjacency, 3, any, wgraph, '../src/wgraph_adjacency').
+lib_index(wgraph_adjacency, 3, any, wgraph, wgraph_adjacency).
+lib_index(wgraph_clique, 2, any, wgraph, '../src/wgraph_clique').
+lib_index(wgraph_clique, 2, any, wgraph, wgraph_clique).
+lib_index(wgraph_clique, 4, any, wgraph, '../src/wgraph_clique').
+lib_index(wgraph_clique, 4, any, wgraph, wgraph_clique).
+lib_index(wgraph_del_vertices, 3, any, wgraph, '../src/wgraph_del_vertices').
+lib_index(wgraph_del_vertices, 3, any, wgraph, wgraph_del_vertices).
+lib_index(wgraph_known_cliques_replace, 4, any, wgraph, '../src/wgraph_known_cliques_replace').
+lib_index(wgraph_known_cliques_replace, 4, any, wgraph, wgraph_known_cliques_replace).
+lib_index(wgraph_plot, 2, any, wgraph, '../src/wgraph_plot').
+lib_index(wgraph_plot, 2, any, wgraph, wgraph_plot).
+lib_index(wgraph_ugraph, 2, any, wgraph, '../src/wgraph_ugraph').
+lib_index(wgraph_ugraph, 2, any, wgraph, wgraph_ugraph).
+lib_index(wgraph_ugraph, 3, any, wgraph, '../src/wgraph_ugraph').
+lib_index(wgraph_ugraph, 3, any, wgraph, wgraph_ugraph).
+lib_index(wgraph_version, 2, any, wgraph, '../prolog/wgraph').
+lib_index(wgraph_vertex_frequencies, 2, any, wgraph, '../src/wgraph_vertex_frequencies').
+lib_index(wgraph_vertex_frequencies, 2, any, wgraph, wgraph_vertex_frequencies).
+lib_index(wgraph_vertices, 2, any, wgraph, '../src/wgraph_vertices').
+lib_index(wgraph_vertices, 2, any, wgraph, wgraph_vertices).
+lib_index(wgraph_vertices_collapse, 4, any, wgraph, '../src/wgraph_vertices_collapse').
+lib_index(wgraph_vertices_collapse, 4, any, wgraph, wgraph_vertices_collapse).
