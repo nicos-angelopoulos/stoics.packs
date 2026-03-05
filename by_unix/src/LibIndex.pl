34db491de4a4633e102f6b562111a9befc91f6c7
Nicos Angelopoulos <nicos@gmx.co.uk>
2026-03-05 01:07:24 +0000

    LIBidx:   for by_unix (created)

---
+++ by_unix/src/LibIndex.pl
@@ -0,0 +1,12 @@
+% date(2026,3,5)
+
+lib_index(&, 1, any, by_unix, '../prolog/by_unix').
+lib_index(@, 1, any, by_unix, '../prolog/by_unix').
+lib_index(@@, 2, any, by_unix, '../prolog/by_unix').
+lib_index(@@, 3, any, by_unix, '../prolog/by_unix').
+lib_index(by_unix_assert, 0, any, by_unix, '../prolog/by_unix').
+lib_index(by_unix_retract, 0, any, by_unix, '../prolog/by_unix').
+lib_index(by_unix_term_to_serial, 2, any, by_unix, '../prolog/by_unix').
+lib_index(by_unix_version, 2, any, by_unix, '../prolog/by_unix').
+lib_index(cd, 2, any, by_unix, '../prolog/by_unix').
+lib_index(which, 2, any, by_unix, '../prolog/by_unix').
